17b3983c594a6802311c8e0efb4c27012d0ca114
Pauline-Humbert <Pauline-Humbert@hotmail.fr>
2020-03-18 15:51:33 +0100

    commentaires de ce qui ne marche pas ^^

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -4,9 +4,9 @@ export class User {
   email: string;
   password: string;
 
-  constructor(userName: string, email: string, password: string){
-    this.userName = userName;
-    this.email = email;
-    this.password = password;
+  constructor(){ // userName: string, email: string, password: string
+    // this.userName = userName;
+    // this.email = email;
+    // this.password = password;
   }
 }
