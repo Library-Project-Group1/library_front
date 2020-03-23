1e8abd401fefdead6da5bf09801db9e67e6b6a53
MambriniBaptiste <mambrini.baptiste@gmail.com>
2020-03-23 16:55:40 +0100

    Manage Category

--- src/app/components/Category/Category-list/category-list.component.ts
+++ src/app/components/Category/Category-list/category-list.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import {Category} from '../../../models/category/category';
 import {CategoryService} from '../../../service/category/category.service';
 import {error} from '@angular/compiler/src/util';
-import {Theme} from '../../../models/theme/theme';
 
 @Component({
   selector: 'app-category-form',
@@ -15,7 +14,6 @@ export class CategoryListComponent implements OnInit {
   @Input()
   categories: Category[];
   category: Category;
-  selectedCategory: Category;
 
   constructor(
     private route: ActivatedRoute,
@@ -28,28 +26,22 @@ export class CategoryListComponent implements OnInit {
     this.categoryService.findAllCategories().subscribe((categories => this.categories = categories),
       error1 => console.error('There are an error!', error1));
   }
-  onSelect(category: Category): void {
-    this.selectedCategory = category;
+  removeCategory(category: Category) {
+    this.categoryService.deleteCategoryById(category.id).subscribe();
+  }
+  addCategory(name: string): void {
+    name = name.trim();
+    if (!name) {return; }
+    this.categoryService.createCategory({name} as Category)
+      .subscribe(category => {
+        this.categories.push(category);
+      });
   }
   editCategory() {
     this.categoryService.updateCategoryById(this.category).subscribe(result => this.goToEditCategory()),
       console.error('There are an error!', error);
   }
-
   goToEditCategory() {
     this.router.navigate(['../category/{id}/editCategory']);
-  }
-
-  removeCategory(category: Category) {
-    this.categoryService.deleteCategoryById(category.id).subscribe();
-  }
-
-  addCategory(name: string): void {
-    name = name.trim();
-    if (!name) {return; }
-    this.categoryService.createCategory({name} as Theme)
-      .subscribe(theme => {
-        this.categories.push(theme);
-      });
   }
 }
