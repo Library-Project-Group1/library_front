cf748c0947a80595031fbe5aacee6bfa0c3d0595
Pauline-Humbert <Pauline-Humbert@hotmail.fr>
2020-03-23 17:17:20 +0100

    update transaction service

--- src/app/service/transaction/transaction.service.ts
+++ src/app/service/transaction/transaction.service.ts
@@ -13,4 +13,19 @@ export class TransactionService {
   constructor(private http: HttpClient) {
   }
 
+  public findTransactionById(transactionId: number): Observable<Transaction> {
+    return this.http.get<Transaction>(this.transactionsUrl + '/transaction/' + transactionId);
+  }
+
+  public findTransactionByUser(): Observable<Transaction> {
+    return this.http.get<Transaction>(this.transactionsUrl + '/transaction/user');
+  }
+
+  public findTransactionByIProduct(): Observable<Transaction> {
+    return this.http.get<Transaction>(this.transactionsUrl + '/transaction/product');
+  }
+
+  public createTransaction(transaction: Transaction) {
+    return this.http.post<Transaction>(this.transactionsUrl + '/createTransaction', transaction, null);
+  }
 }
