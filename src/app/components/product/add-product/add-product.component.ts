f0daedf29df51b231567de9e1e2d6b3a4355bd96
Pauline-Humbert <Pauline-Humbert@hotmail.fr>
2020-03-27 12:40:20 +0100

    form addproduct

--- src/app/components/product/add-product/add-product.component.ts
+++ src/app/components/product/add-product/add-product.component.ts
@@ -4,8 +4,10 @@ import {ProductService} from '../../../service/product/product.service';
 import {Router} from '@angular/router';
 import {Theme} from '../../../models/theme/theme';
 import {Category} from '../../../models/category/category';
-import {Validators} from '@angular/forms';
+import {FormControl, Validators} from '@angular/forms';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {CategoryService} from '../../../service/category/category.service';
+import {ThemeService} from '../../../service/theme/theme.service';
 
 @Component({
   selector: 'app-add-product',
@@ -17,34 +19,61 @@ export class AddProductComponent implements OnInit {
   product: Product;
   products: Product[];
   themes: Theme[];
+  theme: Theme;
   categories: Category[];
+  category: Category;
   fg: FormGroup;
 
   constructor(
     private productService: ProductService,
+    private categoryService: CategoryService,
+    private themeService: ThemeService,
     private router: Router,
     private fb: FormBuilder) {
     this.createForm();
   }
 
   ngOnInit() {
+    this.categoryService.findAllCategories().subscribe((categories => this.categories = categories),
+      error => console.error('There are an error', error));
+    this.themeService.findAllThemes().subscribe((themes => this.themes = themes),
+      error => console.error('There are an error', error));
   }
 
-  addProduct(product: Product) {
-    this.productService.createProduct(this.product).subscribe(result => {
-      this.products.push(product);
-      this.goToProductList();
-    });
+  addProduct(category: number, theme: number, title: string, creator: string, releaseDate: Date,
+             description: string, price: number, quantityTotal: number, quantityAvailableToRent: number, quantityIsRenting: number): void {
+    // title = title.trim();
+    // if (!title) {
+    //   return;
+    // }
+    this.productService.createProduct({
+      category,
+      theme,
+      title,
+      creator,
+      releaseDate,
+      description,
+      price,
+      quantityTotal,
+      quantityAvailableToRent,
+      quantityIsRenting
+    } as unknown as Product).subscribe();
   }
 
   createForm() {
     this.fg = this.fb.group({
-      prod: ['', Validators.required],
+      category: ['', Validators.required],
+      theme: ['', Validators.required],
+      title: ['', Validators.required],
+      creator: ['', Validators.required],
+      releaseDate: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', Validators.required],
+      quantityTotal: ['', Validators.required],
+      quantityAvailable: ['', Validators.required],
+      quantityRented: ['', Validators.required],
     });
   }
 
-  goToProductList() {
-    this.router.navigate(['../allProducts']);
-  }
 
 }
